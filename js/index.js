7adfa9ea7ae66327f29b299b27644cdc83423e8b
adwenpa <joojo.abr@gmail.com>
2025-01-08 17:16:50 -0500

    completed part 1: read the concepts in index.html and experimented inside index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// let healthPoints = 18;
+
+// if (healthPoints >= 10) {
+//     console.log("Hero is healthy.");
+// } else if (healthPoints < 10 && healthPoints >= 5) {
+//     console.log("Hero is slightly damaged.");
+// } else {
+//     console.log("Hero is close to death.");
+// }
+
+
